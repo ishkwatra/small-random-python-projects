deae86e5818a44d50220fa7ec26317c05208b7be
Ish Kwatra <77015916+ishkwatra@users.noreply.github.com>
2023-02-17 19:59:22 +0530

    Create notif.py

---
+++ notif.py
@@ -0,0 +1,17 @@
+import time
+from plyer import notification
+
+
+def take_a_break():
+    notification.notify(
+        title='Time to take a break!',
+        message='Get up, stretch, and look away from your screen for a few minutes.',
+        app_name='Break Reminder',
+        timeout=10
+    )
+
+
+while True:
+    # remind every 30 minutes
+    time.sleep(30*60)
+    take_a_break()
